2e7a33b27c21595f282c62a00fe04b60df2189eb
cumali.turkmenoglu <cumali.turkmenoglu@HITSOFT16>
2024-12-17 15:10:42 +0300

    repo initialization-1

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+SMP Forecasting package.
+This module provides tools for forecasting System Marginal Prices in electricity markets.
+"""
+
+# Version information
+__version__ = '0.1.0'
+__author__ = 'Cumali Türkmenoğlu FSMUNIV '
