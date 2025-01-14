e71626f0ac6fe2f3508427feaf58351423b5c9a0
syakif <akifsy@yandex.com>
2025-01-14 18:23:03 +0300

    extractDate function has been modified

--- src/models/builders.py
+++ src/models/builders.py
@@ -4,12 +4,12 @@ def extractDate(file_path):
 
     # Load the example CSV file
     df = pd.read_csv(file_path)
-
-    # Convert the 'Date' column to datetime
+    print(df)
+    # Convert the 'Date' column to datetime if it exists
     df['Tarih'] = pd.to_datetime(df['Tarih'], format='%d/%m/%Y %H:%M')
 
     # Extract datetime features
-    df['Year'] = df['Tarih'].dt.year
+    df['Year'] = df['Tarih'].dt.year #bu kötü etkiliyor olabilir
     df['Month'] = df['Tarih'].dt.month
     df['Day'] = df['Tarih'].dt.day
     df['Hour'] = df['Tarih'].dt.hour
@@ -17,13 +17,34 @@ def extractDate(file_path):
     # Drop the original "Tarih" column
     #df.drop('Tarih', axis=1, inplace=True)
 
+    df['Tarih'] = df['Tarih'].dt.strftime('%d/%m/%Y %H:%M')
+
     return df
 
 def addRollingStat(file_path, window_size):
 
+    # Step 1: Read the CSV file
     df = pd.read_csv(file_path)
 
-    df["Rolling Mean"] = df["Value"].rolling(window=window_size).mean()
-    df["Rolling Sum"] = df["Value"].rolling(window=window_size).sum()
+    # Step 2: Add rolling statistics
+    df["Rolling Mean"] = df["Smfdolar"].rolling(window=window_size).mean()
+    df["Rolling Sum"] = df["Smfdolar"].rolling(window=window_size).sum()
 
     return df
+
+
+"""
+#Deneme
+file = "with_weather.csv"
+df = extractDate(file)
+
+df.to_csv("deneme.csv", index=False)
+
+file2 = "deneme.csv"
+
+df = addRollingStat(file2, 7)
+
+df.to_csv("deneme.csv", index=False)
+
+print(df)
+"""
